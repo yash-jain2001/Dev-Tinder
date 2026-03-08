973d7b9ea3adba3201a1edb9944243608246c551
Priyanshu jain <89932280+yash-jain2001@users.noreply.github.com>
2026-03-08 12:51:58 +0530

    added user to db dynamically

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ const connectDB = require("./config/database.js");
 const User = require("./models/user");
 const app = express();
 
+
+app.use(express.json());
 app.post("/signup", async (req, res) => {
+// console.log(req.body)
   //creating a new instance of the User model
-  const user = new User({
-    firstName: "virat",
-    lastName: "kohli",
-    email: "[EMAIL_ADDRESS]",
-    password: "[PASSWORD]",
-    age: 35,
-    gender: "male",
-  });
+  const user = new User(req.body);      // data is written in the postman to call a api, when called data is sent as request and recieved by server, then new instance is made and then saved to database
 
   try {
     await user.save();
